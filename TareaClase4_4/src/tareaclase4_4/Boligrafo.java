38795bb5ae1ff4faff237e2af8e65e62e7447433
elyfalcon <elyfalcon@yahoo.com>
2018-10-16 20:16:25 -0300

    consultar el ejercicio4, faltan los metodos

---
+++ TareaClase4_4/src/tareaclase4_4/Boligrafo.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tareaclase4_4;
+import java.awt.Color;
+
+/**
+ *
+ * @author Eli
+ */
+public class Boligrafo {
+    
+    //Color color;
+    private Color _color;
+    private int _tinta;
+    
+    //<editor-fold desc="Getters y Setters">
+    public Color getColor(){
+    return this._color;
+    }
+    public void setColor(Color color){
+    this._color=color;
+    }
+    public int getTinta(){
+    return this._tinta;
+    }
+    public void setTinta(int tinta){
+    this._tinta=tinta;
+    }
+    //</editor-fold>
+    //<editor-fold desc="Metodos">
+    
+    public boolean Pintar(int gasto){
+  
+    }
+    public void Recargar(){
+    
+    }
+    //</editor-fold>
+    
+    
+}
+    
+    
+    
+    
+    
+    
+}
